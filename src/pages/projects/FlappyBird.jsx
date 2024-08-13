57824810bf26fff6ae88d5c3b66f9ebf892eee22
hosua <hosua.o@gmail.com>
2024-08-13 19:54:49 -0400

    Add page for FlapPy Bird

--- src/pages/projects/FlappyBird.jsx
+++ src/pages/projects/FlappyBird.jsx
@@ -1,3 +1,4 @@
+import flappyBirdVideo from "/videos/flapPy-bird.mp4";
 import Video from "@components/Video";
 import { Card } from "react-bootstrap";
 import ProjectPage from "@components/ProjectPage";
@@ -6,11 +7,27 @@ const ProjectFlappyBird = () => {
   const body = () => {
     return (
       <>
-        <Card.Text>TO DO</Card.Text>
+        <Card.Text>
+          FlapPy Bird is a Flappy Bird clone that I created in Python using
+          PyGame. It is played by simply clicking on the screen when you want to
+          flap.
+        </Card.Text>
+        <Card.Text>
+          It features high score saving and uses assets from the original game.
+        </Card.Text>
+        <div style={{ maxWidth: "400px", margin: "auto" }}>
+          <Video src={flappyBirdVideo} />
+        </div>
       </>
     );
   };
-  return <ProjectPage title="FlapPy Bird" body={body()} />;
+  return (
+    <ProjectPage
+      title="FlapPy Bird"
+      body={body()}
+      srcUrl={"https://github.com/hosua/FlapPy-bird"}
+    />
+  );
 };
 
 export default ProjectFlappyBird;
